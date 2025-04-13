e5647da080e84de23d6f7e927bdb27da7fa5cc92
%username% <%email%>
2025-04-13 00:44:31 -0400

    Created Login pages

--- lib/createaccount.dart
+++ lib/createaccount.dart
@@ -26,7 +26,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
+  TextEditingController confirmController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,17 +35,22 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           children: [
             SizedBox(height: 150,),
-            Text("Login Here", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
+            Text("Create Account", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
             SizedBox(height: 15,),
-            Text("Log back in and lets get back on track!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+            SizedBox(
+              width: 300,
+              child: Text("Create an account to get started. It’s quick and easy!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15), textAlign: TextAlign.center,),
+            ),
             SizedBox(height: 50,),
             Padding(
-                padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+              padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
               child: Column(
                 children: [
                   _inputField(emailController, "Email", false),
                   SizedBox(height: 20,),
-                  _inputField(passwordController,"Password", true),
+                  _inputField(passwordController, "Password", true),
+                  SizedBox(height: 20,),
+                  _inputField(confirmController, "Confirm Password", true),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
@@ -56,9 +61,9 @@ class _LoginPageState extends State<LoginPage> {
                     ],
                   ),
                   SizedBox(height: 40,),
-                  _button("Sign in", Colors.black, Color(0xFFEEFFFF),),
+                  _button("Sign Up", Colors.black, Color(0xFFEEFFFF), 17.0),
                   SizedBox(height: 20,),
-                  _button("Create new Account",Color(0xFFEEFFFF), Colors.black)
+                  _button("Already have an account?",Color(0xFFEEFFFF), Colors.black, 13.0),
                 ],
               ),
             ),
@@ -69,11 +74,11 @@ class _LoginPageState extends State<LoginPage> {
               width: 50,
               height: 50,
               decoration: BoxDecoration(
-                color: Color(0xFFEEFFFF),
-                borderRadius: BorderRadius.circular(5)
+                  color: Color(0xFFEEFFFF),
+                  borderRadius: BorderRadius.circular(5)
               ),
               child: Padding(
-                  padding: EdgeInsets.all(5),
+                padding: EdgeInsets.all(5),
                 child: Image.asset("assets/google.png", width: 10, height: 10,),
               ),
             ),
@@ -99,7 +104,7 @@ class _LoginPageState extends State<LoginPage> {
       child: TextField(
         controller: controller,
         decoration: InputDecoration(
-          labelStyle: TextStyle(color: Colors.grey),
+            labelStyle: TextStyle(color: Colors.grey),
             labelText: message,
             fillColor: Color(0xFFEEFFFF),
             filled: true,
@@ -107,34 +112,35 @@ class _LoginPageState extends State<LoginPage> {
                 borderRadius: BorderRadius.circular(5),
                 borderSide: BorderSide.none
             )
+
         ),
         obscureText: obscure,
       ),
     );
   }
 
-  Widget _button(message, buttonColor, textColor) {
+  Widget _button(message, buttonColor, textColor, textSize) {
     return Container(
       height: 40,
       width: 250,
       decoration: BoxDecoration(
-        color: buttonColor,
-        borderRadius: BorderRadius.circular(5),
-        boxShadow: [
-          BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              blurRadius: 3,
-              offset: Offset(0, 4)
-          )
-        ]
+          color: buttonColor,
+          borderRadius: BorderRadius.circular(5),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 3,
+                offset: Offset(0, 4)
+            )
+          ]
       ),
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: buttonColor,
-            elevation: 0
-        ),
+          style: ElevatedButton.styleFrom(
+              backgroundColor: buttonColor,
+              elevation: 0
+          ),
           onPressed: () {},
-          child: Text(message, style: TextStyle(color: textColor, fontSize: 17),)
+          child: Text(message, style: TextStyle(color: textColor, fontSize: textSize),)
       ),
     );
   }
